46c0eec8e0854a9eb24b813faa072c6682019895
Testing <info@ahead-itec.com>
2024-09-23 21:10:40 +0200

    APDUSDK v2.0.1

---
+++ APDUSDK.xcframework/APDUSDK.xcframework/ios-arm64/APDUSDK.framework/Headers/APDUSDK.h
@@ -0,0 +1,18 @@
+//
+//  APDUSDK.h
+//  APDUSDK
+//
+//  Created by Ivan Gurtler on 24/05/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for APDUSDK.
+FOUNDATION_EXPORT double APDUSDKVersionNumber;
+
+//! Project version string for APDUSDK.
+FOUNDATION_EXPORT const unsigned char APDUSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <APDUSDK/PublicHeader.h>
+
+
